d2e52e950adaac244a631ab21697ad3ce59fc773
sappidisupriya630-star <sappidisupriya630@gmail.com>
2026-03-03 11:26:46 +0530

    Add Task model with SQLAlchemy integration

---
+++ models.py
@@ -0,0 +1,13 @@
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(500))
+    completed = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f"<Task {self.title}>"
